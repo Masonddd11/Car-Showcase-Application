eb0d35868c304b8eadf7b8ccc2fb9a281ea682d6
Mason WONG <fpsmason2020@gmail.com>
2024-02-22 20:53:16 +0800

    Finished

---
+++ components/CarCard.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import { CarProps } from "@/constants";
+import CustomButton from "./CustomButton";
+import { calculateCarRent } from "@/utils";
+import CarDetails from "./CarDetails";
+
+const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+
+  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+  url.searchParams.append('make', make);
+  if (typeof model === 'string') {
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+  }
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
+} 
+
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
+  const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+    return rentalRatePerDay.toFixed(0);
+  };
+
+  const CarRent = calculateCarRent(city_mpg, year);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+
+      <p className="flex mt-6 text-[32px] font-semibold">
+        <span className="self-start text-[16px] font-semibold">$</span>
+        {CarRent}
+        <span className="self-start text-[16px] font-medium">/day</span>
+      </p>
+
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src={generateCarImageUrl(make, model)}
+          alt="car model"
+          fill
+          objectFit="contain"
+          priority
+        />
+      </div>
+
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              width={20}
+              height={20}
+              alt="steering wheel"
+            />
+            <p className="text-[14px]">
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/gas.svg" width={20} height={20} alt="gas" />
+            <p className="text-[14px]">{city_mpg}</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tire.svg" width={20} height={20} alt="tire" />
+            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          </div>
+        </div>
+
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
+      </div>
+
+      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car} />
+    </div>
+  );
+};
+
+export default CarCard;
---
+++ components/ShowMore.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { ShowMoreProps } from "@/types";
+import CustomButton from "./CustomButton";
+
+const updateSearchParams = (type: string, value: string) => {
+  const searchParams = new URLSearchParams(window.location.search);
+
+  searchParams.set(type, value);
+
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+  return newPathname;
+};
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = (e) => {
+    e.preventDefault();
+    
+    const newLimit = (pageNumber + 1) * 10;
+
+    const newPathname = updateSearchParams("limit", `${newLimit}`);
+
+    router.push(newPathname).then(() => window.scrollTo(0, document.body.scrollHeight));
+  };
+
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          btnType="button"
+          title="Show More"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ShowMore;
