dd6adfa713ca0b5dfe384544c0dbaa313ed36390
Mason WONG <fpsmason2020@gmail.com>
2024-02-23 00:29:29 +0800

    h

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -4,7 +4,7 @@ import { Fragment } from "react";
 import Image from "next/image";
 
 import { Dialog, Transition } from "@headlessui/react";
-import { CarProps } from "@types";
+import { CarProps } from "@/types";
 import { Gasoek_One } from "next/font/google";
 
 interface CarDetailsProps {
